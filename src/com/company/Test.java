b438b437e5e550e3439b60ede727c058a79806e0
“Max” <maxmyway@gmail.com>
2016-04-09 11:44:52 +0300

    add Test

---
+++ src/com/company/Test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by MaxS on 4/9/16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hellow world!");
+    }
+}
